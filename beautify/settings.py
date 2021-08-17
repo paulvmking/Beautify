ba873ced61baff439e2ed10e2ad57c7cde77a1a9
paulvmking <paulvmking@gmail.com>
2021-08-17 12:00:10 +0000

    removed secret key and set debug

--- beautify/settings.py
+++ beautify/settings.py
@@ -23,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-acqit0mf+i9igu*7w9m8&%b+*o%rh*do!o=#ck0(r&nr35%43z'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['beautify-store.herokuapp.com', 'localhost']
 
